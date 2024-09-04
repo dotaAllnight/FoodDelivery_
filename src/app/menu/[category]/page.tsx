f787a9e09c164e91ab52acd292aea64f5dc10f18
superXD <maxzatoplane@gmail.com>
2024-09-04 15:27:07 +0700

    version_023

---
+++ src/app/menu/[category]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Categoey = () => {
+    return (
+        <div>Categoey</div>
+    )
+}
+
+export default Categoey
---
+++ src/app/menu/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Menupage = () => {
+    return (
+        <div>MenuPage</div>
+    )
+}
+
+export default Menupage
