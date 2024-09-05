2c9e33ca3aa32c6b35748b426d06810ef5c44c09
superXD <maxzatoplane@gmail.com>
2024-09-05 17:05:23 +0700

    save

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,12 +1,31 @@
-import { pizzas } from '@/data'
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
 
-const Categoey = () => {
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
+import { ProductType } from '@/types/types';
+
+const getData = async (category: string) => {
+    const res = await fetch(` http://localhost:3000/api/products?cat=${category}`, {
+        cache: "no-store"
+    })
+
+    if (!res.ok) {
+        throw new Error("Failed");
+    }
+
+    return res.json()
+}
+
+type Props = {
+    params: { category: string }
+}
+
+const CategoryPage = async ({ params }: Props) => {
+
+    const products: ProductType[] = await getData(params.category)
     return (
         <div className='flex flex-wrap text-red-500'>
-            {pizzas.map((item) => (
+            {products.map((item) => (
                 <Link className="w-full h-[60vh] border-r-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group even:bg-red-100" href={`/product/${item.id}`} key={item.id}>
                     {/*IMAGE CONTAINER*/}
                     {item.img && (
@@ -26,4 +45,4 @@ const Categoey = () => {
     )
 }
 
-export default Categoey
+export default CategoryPage
